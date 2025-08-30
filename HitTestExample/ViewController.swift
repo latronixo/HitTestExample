27642a6e96613af50fe9b10fdba3491b2367bafc
Valentin <pamerra@mail.ru>
2025-08-30 21:01:28 +0300

    Initial Commit

---
+++ HitTestExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HitTestExample
+//
+//  Created by Валентин on 30.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
